68ea8e924669fd115d31f0eff9e8092294c32fe6
Sean Conroy <seanconroy2001@gmail.com>
2022-09-05 20:02:35 +0100

    Connect to database

--- src/main.py
+++ src/main.py
@@ -1,14 +1,36 @@
 import os
 from flask import Flask, redirect, render_template
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 load_dotenv()
 
 app = Flask(__name__)
 
+## Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+## Configure Tables
+class Customer(db.Model):
+  __tablename__ = "customers"
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(250), unique=True, nullable=False)
+  industry = db.Column(db.String(250), nullable=False)
+  date = db.Column(db.String(250), nullable=False)
+
+db.create_all()
+
 @app.route('/')
 def home():
    return render_template("index.html", title="Home")
+
+@app.route('/customer')
+def get_customers():
+    customers = Customer.query.all()
+    return render_template("customers.html", title="Customers", all_customers=customers)
 
 # Error Handlers
 @app.errorhandler(404)
